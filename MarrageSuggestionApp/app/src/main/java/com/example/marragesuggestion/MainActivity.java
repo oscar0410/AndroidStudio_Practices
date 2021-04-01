0d2e8b080bec8102baba996f4379b2a6cf431c20
Oscar <a127309512@gmail.com>
2021-04-02 01:20:24 +0800

    #comment 使用下拉式選單取代輸入欄位

--- MarrageSuggestionApp/app/src/main/java/com/example/marragesuggestion/MainActivity.java
+++ MarrageSuggestionApp/app/src/main/java/com/example/marragesuggestion/MainActivity.java
@@ -4,17 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     // EditText物件
-    private EditText mEdtGender, mEdtAge;
+    private EditText mEdtAge;
     // TextView物件
     private TextView mTxtResult;
     // Button物件
     private Button mBtnDo;
+    // 性別
+    private String msGender;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +27,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // 連結介面元件
-        mEdtGender = findViewById(R.id.edtGender);
+
         mEdtAge = findViewById(R.id.edtAge);
         mTxtResult = findViewById(R.id.textResult);
         mBtnDo = findViewById(R.id.btnDo);
 
         // 設定按鈕觸發事件
         mBtnDo.setOnClickListener(btnDoOnClick);
+
+        Spinner spnGender = findViewById(R.id.spnGender);
+
+        spnGender.setOnItemSelectedListener(spnGenderOnItemSelected);
     }
     private View.OnClickListener btnDoOnClick = new View.OnClickListener(){
         @Override
         public void onClick(View v){
-            // 取得性別和年齡
-            String sex = mEdtGender.getText().toString();
+            // 取得年齡
             int age = Integer.parseInt(mEdtAge.getText().toString());
             // 從字串資源取出預先定義好的suggestion字串
             String suggestion = getString(R.string.suggestion);
 
             //判斷性別
-            if(sex.equals(R.string.male)){
+            if(msGender.equals(getString(R.string.male))){
                 if(age<28){
                     suggestion += getString(R.string.not_hurry);
                 }else if(age>33){
@@ -61,5 +69,25 @@ public class MainActivity extends AppCompatActivity {
             // 輸出結果
             mTxtResult.setText(suggestion);
         }
+    };
+
+    private AdapterView.OnItemSelectedListener spnGenderOnItemSelected = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            switch (position){
+                case 0:
+                    msGender = getString(R.string.male);
+                    break;
+                case 1:
+                    msGender = "";
+                    break;
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+
     };
 }
